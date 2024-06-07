dfb7b435e77d616acf7a4cb438bc5867f49a0fbc
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-07 18:25:10 +0500

    api

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
-const url = "https//www.codewithharry.com"
-fetch(url, {
-    name: "name",
-    value: "value"
-});
-fetch();
+fetch('https://dummyjson.com/auth/RESOURCE', {
+  method: 'GET', 
+  headers: {
+    'Authorization': 'Bearer /* YOUR_TOKEN_HERE */', 
+    'Content-Type': 'application/json'
+  }, 
+})
+.then(res => res.json())
+.then(console.log);
